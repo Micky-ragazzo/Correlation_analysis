7760991eadedcd7635d6d29738c898767377d4d2
Micky-ragazzo <Micky-ragazzo@users.noreply.github.com>
2019-04-23 11:23:02 +0200

    Add diagram

---
+++ R_main.R
@@ -0,0 +1,13 @@
+#colnames(data)[11] <- "R_SH"
+library("ggplot2", lib.loc="~/R/R-3.5.3/library")
+library(psycho)
+library(tidyverse)
+#visualize distribution
+z_df_choose %>% 
+  +     dplyr::select(-input_DMC, -MD1_ANLIEF_MG, -Wuchtvers_anz, R_SH, -MD2_ANLIEF_G) %>% 
+  +     gather(Variable, Value) %>% 
+  +     ggplot(aes(x=Value, fill=Variable)) +
+  +     geom_density(alpha=0.4) +
+  +     geom_vline(aes(xintercept=0)) +
+  +     theme_light() +
+  +     scale_fill_brewer(palette="Spectral")
