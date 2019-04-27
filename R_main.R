f52cc4ca90ada7a28fb7f73f07679d7cd48fdfd4
Micky-ragazzo <Micky-ragazzo@users.noreply.github.com>
2019-04-27 15:41:15 +0200

    Commit (add linear)

--- R_main.R
+++ R_main.R
@@ -5,6 +5,8 @@ library("ggplot2", lib.loc="~/R/R-3.5.3/library")
 library(psycho)
 library(tidyverse)
 library(dplyr)
+library(mgcv)
+
 #---------------------------------
 #Standarize data
 # z_df <- df %>% 
@@ -29,3 +31,12 @@ bstats <- boxplot(count ~ spray, data = data, col = "lightgray")
 bstats$out <- NULL
 bstats$group <- NULL
 bxp(bstats)  # this will plot without any outlier points
+#linear approach model z_df - RS 
+lm_mod <- lm(R_SH ~ Wuchtvers_anz, data = df)
+ termplot(lm_mod, partial.resid = TRUE, se = TRUE)
+
+#linear approach model z_schaft
+ lm_mod <- lm(Final_2 ~ Wuchtvers_anz, data = z_schaft)
+ termplot(lm_mod, partial.resid = TRUE, se = TRUE)
+ 
+ 
